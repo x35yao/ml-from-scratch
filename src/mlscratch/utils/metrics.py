6364784279cab20c0e64862ce44e72c34f735afd
Luke <x35yao@uwaterloo.ca>
2025-09-20 15:19:04 -0400

    add node_mse and node_mae metrics

--- src/mlscratch/utils/metrics.py
+++ src/mlscratch/utils/metrics.py
@@ -8,3 +8,15 @@ def entropy(p):
     p = np.asarray(p, dtype=float).ravel()
     m = p > 0  # mask to avoid log(0)
     return -np.sum(p[m] * np.log2(p[m]))  # use log2 for bits
+
+def node_mse(y):
+    """Mean Squared Error of a node."""
+    y = np.asarray(y, dtype=float)
+    y_hat = np.mean(y)
+    return np.mean((y - y_hat) ** 2)
+
+def node_mae(y):
+    """Mean Absolute Error of a node."""
+    y = np.asarray(y, dtype=float)
+    y_hat = np.median(y)
+    return np.mean(np.abs(y - y_hat))
